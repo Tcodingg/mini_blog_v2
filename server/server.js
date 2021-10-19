f7b6fdcffb3e39c5760495ce974fec4d3f882ea0
Tcodingg <princetlive@yahoo.com>
2021-10-19 15:30:50 -0400

    setup the backend server and connect to mongdb database

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json({ limit: "50mb" }));
+app.use(cors());
+
+const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@crud.rpq4m.mongodb.net/miniBlog?retryWrites=true`;
+mongoose.connect(
+   url,
+   { useNewUrlParser: true, useUnifiedTopology: true },
+   (err, db) => {
+      if (err) {
+         console.log("error" + err);
+      } else {
+         console.log("connected to database");
+      }
+   }
+);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+   console.log(`server is running on port ${PORT}`);
+});
