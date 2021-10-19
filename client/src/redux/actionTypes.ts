50244a1c772af5536d8273fa25308e5b4439839d
Tcodingg <princetlive@yahoo.com>
2021-10-19 15:47:03 -0400

    create types for all action types

--- client/src/redux/actionTypes.ts
+++ client/src/redux/actionTypes.ts
@@ -4,3 +4,34 @@ export enum actionTypes {
    CREATE_POST = "CREATE_POST",
    LIKE_POST = "LIKE_POST",
 }
+
+type actionGetPost = {
+   type: actionTypes.GET_POST;
+   payload: posts[];
+};
+
+export interface posts {
+   _id: Number;
+   title: string;
+   note: string;
+   img: string;
+   likes: Number;
+}
+
+type actionDeletePost = {
+   postId: Number;
+};
+
+type actionCreatePost = {
+   post: posts;
+};
+
+type actionLikePost = {
+   postId: Number;
+};
+
+export type Actions =
+   | actionGetPost
+   | actionDeletePost
+   | actionCreatePost
+   | actionLikePost;
