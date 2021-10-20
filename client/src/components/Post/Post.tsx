54a7cb65805ccb3885e034f51012b5a9bab464cb
Tcodingg <princetlive@yahoo.com>
2021-10-20 13:58:31 -0400

    remove the dummy post  from Posts and pass props to the Post

--- client/src/components/Post/Post.tsx
+++ client/src/components/Post/Post.tsx
@@ -3,22 +3,19 @@ import "./Post.css";
 
 import { IoMdThumbsUp } from "react-icons/io";
 import { MdDelete } from "react-icons/md";
-const Post: React.FC = () => {
+
+import { post } from "../../redux/actionTypes";
+
+const Post: React.FC<post> = ({ title, note, likes, img, _id }) => {
    return (
-      <div className="posts-container">
-         <h5>Rock Climbing</h5>
-         <img
-            src="https://cdn.thecoolist.com/wp-content/uploads/2021/03/Rock-climbing-terminology.jpg"
-            alt="...loading"
-         />
-         <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
-            dicta alias est id sunt. Iusto dolore nemo iste eaque ipsa!
-         </p>
+      <div key={_id} className="posts-container">
+         <h5>{title}</h5>
+         <img src={img} alt="...loading" />
+         <p>{note}</p>
          <div className="icons-container">
             <div className="thumbup-counter icons">
                <IoMdThumbsUp className="thumb-up " />
-               <p style={{ fontSize: "10px" }}>10</p>
+               <p style={{ fontSize: "10px" }}>{likes}</p>
             </div>
             <MdDelete className="delete icons" />
          </div>
--- client/src/components/Posts/Posts.tsx
+++ client/src/components/Posts/Posts.tsx
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Posts.css";
 import Post from "../Post/Post";
 
+import { useDispatch, useSelector } from "react-redux";
+import { getPost } from "../../redux/action";
+import { RootState } from "../../redux/rootState";
 const Posts: React.FC = () => {
+   const dispatch = useDispatch();
+   useEffect(() => {
+      dispatch(getPost());
+   }, [dispatch]);
+
+   const posts = useSelector((state: RootState) => state.reducer);
+
    return (
       <div className="posts">
-         <Post />
-         <Post />
-         <Post />
-         <Post />
-         <Post />
-         <Post />
-         <Post />
+         {posts.map((post) => {
+            return (
+               <Post
+                  title={post.title}
+                  note={post.note}
+                  likes={post.likes}
+                  img={post.img}
+                  _id={post._id}
+               />
+            );
+         })}
       </div>
    );
 };
